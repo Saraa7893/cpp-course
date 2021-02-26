763d73632f36b3f49dcc123f1b5f128404ed9c0a
Saraa7893 <78836178+Saraa7893@users.noreply.github.com>
2021-02-26 11:09:21 -0600

    Add files via upload

---
+++ 1TOinput.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+   cout<<"Enter number: ";
+   cin>>x;
+   for(int n = 1; n<=x; n++){
+       cout<<n<<" "<<endl;
+   }
+   
+	
+	return 0;
+}
