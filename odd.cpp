9399a9319cd02d1050d67d6430ee7d7c8c0c2ace
Saraa7893 <78836178+Saraa7893@users.noreply.github.com>
2021-02-27 12:24:04 -0600

    Add files via upload

---
+++ odd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    cout<<"Enter number: ";
+    cin>>x;
+    for(int n = 0; n<=x; n++){
+        if(n%2!=0){
+            cout<<n<<" ";
+        }
+    }
+	return 0;
+}
